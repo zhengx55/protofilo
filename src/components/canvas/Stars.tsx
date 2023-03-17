5237b3e341ecb9009f6bd823416a38f35d4abddb
zhengx55 <williamzhxin@gmail.com>
2023-03-16 21:34:44 -0400

    ts-nocheck

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -1,10 +1,11 @@
 import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
+//@ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props: any) => {
-  const ref = useRef();
+  const ref = useRef<any>();
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
